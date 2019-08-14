6f36be5b2c2f49e2e5d39fc9ad03ef549318fc3a
Justin <justindchan@gmail.com>
2019-08-14 16:26:17 -0700

    babel running

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,34 @@
+class App extends React.Component {
+  constructor(props) {
+    super (props);
+
+  }
+
+
+  F1 () {
+
+  }
+
+  F2 () {
+
+  }
+
+  F3 () {
+    
+  }
+
+  render() {
+
+    return (
+      <div> "Hello World" </div>
+    )
+  }
+}
+
+
+export default App
+
+
+
 
 
---
+++ challenge_3/public/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _app = require('./app.jsx');
+
+var _app2 = _interopRequireDefault(_app);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_reactDom2.default.render(_react2.default.createElement(_app2.default, null), document.getElementById('app'));
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2NsaWVudC9pbmRleC5qc3giXSwibmFtZXMiOlsiUmVhY3RET00iLCJyZW5kZXIiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIl0sIm1hcHBpbmdzIjoiOztBQUFBOzs7O0FBQ0E7Ozs7QUFDQTs7Ozs7O0FBRUFBLG1CQUFTQyxNQUFULENBQWdCLDhCQUFDLGFBQUQsT0FBaEIsRUFBd0JDLFNBQVNDLGNBQVQsQ0FBd0IsS0FBeEIsQ0FBeEIiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnO1xuaW1wb3J0IFJlYWN0RE9NIGZyb20gJ3JlYWN0LWRvbSc7XG5pbXBvcnQgQXBwIGZyb20gJy4vYXBwLmpzeCdcblxuUmVhY3RET00ucmVuZGVyKDxBcHAvPiwgZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ2FwcCcpKSJdfQ==
